62ca73146882f214bd2c319f5e8ee257f9866a4b
tsuyuri-n <t.suzuki.9657@gmail.com>
2023-12-09 20:44:23 +0900

    TODO追加機能実装

--- vanilla-js-todo-app/index.js
+++ vanilla-js-todo-app/index.js
@@ -1,7 +1,29 @@
-
+/**
+ * TODOを追加する
+ */
 const onClickAdd = () => {
+  // テキストボックスの値を取得し、初期化する
   const inputText = document.getElementById("add-text").value;
-  alert(inputText);
+  document.getElementById("add-text").value = "";
+
+  // li生成
+  const li = document.createElement("li");
+
+  // div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // p生成
+  const p = document.createElement("p");
+  p.className = "todo-item";
+  p.innerText = inputText;
+
+  // 生成したタグをネスト
+  div.appendChild(p);
+  li.appendChild(div);
+
+  // 未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(li);
 };
 
 document.getElementById("add-button").addEventListener("click", onClickAdd);
