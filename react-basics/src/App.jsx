3d12e77bc21ce791328ca05f49b5cea7d103e751
tsuyuri-n <t.suzuki.9657@gmail.com>
2023-12-15 22:01:56 +0900

    useStateの実装

--- react-basics/src/App.jsx
+++ react-basics/src/App.jsx
@@ -1,14 +1,19 @@
-import './App.css';
+import { useState } from 'react';
 import { ColorfulMessage } from './conponents/ColorfulMessage';
 
 function App() {
-  const onClickBtn = () => alert();
+  const [num, setNum] = useState(0);
+  const onClickCountUp = () => {
+    setNum((prev) => prev + 1); // ←複数行同じ処理しても適用される
+    setNum(num + 1); // ←複数行記述してもnumの値は1回のみ
+  };
   return (
     <>
       <h1 style={{ color: "lightcoral" }}>Hello</h1>
       <ColorfulMessage color="lightskyblue">What's up?</ColorfulMessage>
       <ColorfulMessage color="lightgreen">Awsome!</ColorfulMessage>
-      <button onClick={onClickBtn}>ボタン</button>
+      <button onClick={onClickCountUp}>count up</button>
+      <p>{num}</p>
     </>
   );
 }
