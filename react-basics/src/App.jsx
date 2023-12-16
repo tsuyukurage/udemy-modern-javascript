08ef7bfae05f3846c85eaf6b5429b9c1acf22708
tsuyuri-n <t.suzuki.9657@gmail.com>
2023-12-16 23:37:32 +0900

    再レンダリング・useEffect追加

--- react-basics/src/App.jsx
+++ react-basics/src/App.jsx
@@ -1,12 +1,30 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { ColorfulMessage } from './conponents/ColorfulMessage';
 
 function App() {
+  console.log('--App--');
   const [num, setNum] = useState(0);
+  const [isShowCat, setIsShowCat] = useState(true);
   const onClickCountUp = () => {
     setNum((prev) => prev + 1); // ←複数行同じ処理しても適用される
     setNum(num + 1); // ←複数行記述してもnumの値は1回のみ
   };
+  const onClickToggle = () => {
+    setIsShowCat(!isShowCat);
+  };
+
+
+  useEffect(() => {
+    console.log("--useEffect--")
+    if (num > 0) {
+      if (num % 3 === 0) {
+        isShowCat || setIsShowCat(true);
+      } else {
+        isShowCat && setIsShowCat(false);
+      }  
+    }  
+  }, [num]);
+
   return (
     <>
       <h1 style={{ color: "lightcoral" }}>Hello</h1>
@@ -14,6 +32,9 @@ function App() {
       <ColorfulMessage color="lightgreen">Awsome!</ColorfulMessage>
       <button onClick={onClickCountUp}>count up</button>
       <p>{num}</p>
+      <button onClick={onClickToggle}>on/off</button>
+      {isShowCat && <p>(=^ェ^=)</p>}
+      
     </>
   );
 }
--- react-basics/src/conponents/ColorfulMessage.jsx
+++ react-basics/src/conponents/ColorfulMessage.jsx
@@ -1,7 +1,8 @@
 export const ColorfulMessage = ({ color, children }) => { // 引数の方で分割代入
+  console.log("--ColorfulMessage--")
   // 分割代入
   // const { color, children } = props;
-  const contentStyle = { 
+  const contentStyle = {
     color, // color: color, は省略できる
     fontSize: "18px",
   };
