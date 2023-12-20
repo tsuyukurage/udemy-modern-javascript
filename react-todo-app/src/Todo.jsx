12d52ab9640a475c47faf22a2cb9deda2fa0fa01
tsuyuri-n <t.suzuki.9657@gmail.com>
2023-12-20 23:37:12 +0900

    JSXで構造の実装

--- react-todo-app/src/Todo.jsx
+++ react-todo-app/src/Todo.jsx
@@ -4,9 +4,30 @@ import './App.css';
 export const Todo = () => {
   return (
     <>
-      <div></div>
-      <div></div>
-      <div></div>
+      <div>
+        <input type="text" placeholder='TODOを入力' />
+        <button>追加</button>
+      </div>
+      <div>
+        <p>未完了のTODO</p>
+        <ul>
+          <li>
+            <p>TODOです。</p>
+            <button>完了</button>
+            <button>削除</button>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <p>完了のTODO</p>
+        <ul>
+          <li>
+            <p>TODOでした</p>
+            <button>戻す</button>
+          </li>
+        </ul>
+
+      </div>
     </>
   );
 }
