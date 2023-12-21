d066f47d2608862474caeae8b65dbff89a39d7da
tsuyuri-n <t.suzuki.9657@gmail.com>
2023-12-21 22:29:36 +0900

    CSS記述

--- react-todo-app/src/Todo.jsx
+++ react-todo-app/src/Todo.jsx
@@ -4,26 +4,30 @@ import './App.css';
 export const Todo = () => {
   return (
     <>
-      <div>
+      <div className='input-area'>
         <input type="text" placeholder='TODOを入力' />
         <button>追加</button>
       </div>
-      <div>
-        <p>未完了のTODO</p>
+      <div className='incomplete-area'>
+        <p className='title'>未完了のTODO</p>
         <ul>
           <li>
-            <p>TODOです。</p>
+            <div className='list-row'>
+            <p className='todo-item'>TODOです。</p>
             <button>完了</button>
             <button>削除</button>
+            </div>
           </li>
         </ul>
       </div>
-      <div>
-        <p>完了のTODO</p>
+      <div className='complete-area'>
+        <p className='title'>完了のTODO</p>
         <ul>
           <li>
-            <p>TODOでした</p>
-            <button>戻す</button>
+            <div className='list-row'>
+              <p className='todo-item'>TODOでした</p>
+              <button>戻す</button>
+            </div>
           </li>
         </ul>
 
