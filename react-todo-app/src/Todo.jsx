c4f0773eb3ae6541db6d8088c1f8e3bb5a1a3bd9
tsuyuri-n <t.suzuki.9657@gmail.com>
2023-12-26 22:00:59 +0900

    タスク削除機能実装

--- react-todo-app/src/Todo.jsx
+++ react-todo-app/src/Todo.jsx
@@ -19,9 +19,14 @@ export const Todo = () => {
     if (todoText === "") return;
     const newTodos = [...incompleteTodos, todoText];
     setIncompleteTodos(newTodos);
+    setTodoText("");
   };
   // const onClickComplete = () => {};
-  // const onClickRemove = () => {};
+  const onClickDelete = (index) => {
+    const newTodos = [...incompleteTodos];
+    newTodos.splice(index, 1);
+    setIncompleteTodos(newTodos);
+  };
   // const onClickReturn = () => {};
 
   return (
@@ -33,12 +38,12 @@ export const Todo = () => {
       <div className='incomplete-area'>
         <p className='title'>未完了のTODO</p>
         <ul>
-          {incompleteTodos.map((todo) => (
+          {incompleteTodos.map((todo, index) => (
             <li key={todo}>
               <div className='list-row'>
                 <p className='todo-item'>{todo}</p>
                 <button>完了</button>
-                <button>削除</button>
+                <button onClick={() => onClickDelete(index)}>削除</button>
               </div>
             </li>
           ))}
